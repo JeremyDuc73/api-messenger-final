eff18d05e99e315916f5d5c7f03aaf5f2f06f886
jeremyduc <duc.jeremy@sfr.fr>
2022-11-18 16:50:48 +0100

    end 18/11 17h

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const baseURL = "https://localhost:8000/"
+const baseURL = "https://172.104.149.64/"
 
 
 
@@ -8,7 +8,7 @@ const mainContainer = document.querySelector("#main")
 const messagesPageButton = document.querySelector("#messagesPage")
 const registerPageButton = document.querySelector("#registerPage")
 const loginPageButton = document.querySelector("#loginPage")
-
+const registerAndLoginButtons = document.querySelector("#registerAndLoginButtons")
 let token = null
 
 
@@ -24,7 +24,6 @@ function clearMainContainer(){
     mainContainer.innerHTML= ""
 }
 
-
 function display(content){
     //vider la div principale
     clearMainContainer()
@@ -36,9 +35,11 @@ function display(content){
 function getMessageTemplate(message){
 
     let template = `
-                            <div class="row border border-dark">
+                            <div class="border border-dark">
                                 <p>Author : ${message.author.username}</p>
-                                <p><strong>${message.content}</strong></p>
+                                <div class="messageContent">
+                                    <p><strong>${message.content}</strong></p>
+                                </div>
                             </div>
                         `
 
@@ -61,12 +62,11 @@ function getMessagesTemplate(messages){
 
 function getMessageFieldTemplate(){
     let template = `
-         <div class="row messageForm">
-
-                <input type="text" name="" id="messageField" placeholder="input message">
-                <button class="btn btn-success" id="sendMessage">Send</button>
-
-        </div>`
+         <nav class="navbar fixed-bottom bg-dark messageForm container d-flex justify-content-center">
+            <input class="newMessageField pt-2 pb-2" type="text" id="messageField" placeholder="input message">
+            <button class="btn btn-primary pt-2 pb-2 ps-4 pe-4 ms-2" id="sendMessage"><i class="bi bi-send"></i></button>
+        </nav>
+        `
 
     return template
 }
@@ -74,24 +74,36 @@ function getMessageFieldTemplate(){
 function getRegisterTemplate(){
     let template = `
                 <div class="register container">
-                        <input type="text" id="regUsername" placeholder="username">
-                        <input type="password" id="regPassword" placeholder="password">
-                        <button class="btn btn-primary" id="register">register</button>
-            
-                 </div>
-    `
+                    <input type="text" id="regUsername" placeholder="username">
+                    <input type="password" id="regPassword" placeholder="password">
+                    <button type="submit" class="btn btn-primary" id="register">register</button>
+                </div>
+                `
     return template
 }
 
 function getLoginTemplate(){
-    let template = `    <div class="container">
-                            <h2>Log In</h2>
-                            <input type="text" name="" id="usernameLogin" placeholder="username">
-                            <input type="password" name="" id="passwordLogin" placeholder="password">
-                            <button class="btn btn-primary" id="loginButton">log in</button>
-                    
-                        </div>`
-
+    let template = `    
+    <div class="d-flex justify-content-center flex-column align-items-center ">
+        <div class="loginPage text-center p-4">
+            <h2>Log In</h2>
+            <div class="group">
+                <input type="text" id="usernameLogin">
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Name</label>
+            </div>
+            <div class="group">
+                <input type="password" id="passwordLogin">
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Email</label>
+            </div>
+            <button class="btn btn-primary" id="loginButton">log in</button>
+        </div>
+        
+    </div>
+`
     return template
 }
 
@@ -121,29 +133,26 @@ async function displayMessagesPage(){
 
     if(!token){
         displayLoginPage()
-        alert("log in to see messages")
+        const modalNeedToLogin = document.querySelector("#modalNeedToLogin")
+        const modal = new bootstrap.Modal(modalNeedToLogin)
+        modal.show()
     }else{
-
-
-
         let messagesAndMessageField = ""
-
         getMessagesFromApi().then(messages=>{
-
-
             messagesAndMessageField+=getMessagesTemplate(messages)
             messagesAndMessageField+=getMessageFieldTemplate()
-
             display(messagesAndMessageField)
-
             const messageField = document.querySelector("#messageField")
-
             const sendButton = document.querySelector("#sendMessage")
             sendButton.addEventListener("click", sendMessage)
-
-
         })
     }
+}
+
+function modalEmptyUsernameOrPassword(){
+    const modalEmptyUsernameOrPassword = document.querySelector("#modalUsernameOrPasswordEmpty")
+    const modal = new bootstrap.Modal(modalEmptyUsernameOrPassword)
+    modal.show()
 }
 
 function displayLoginPage(){
@@ -152,7 +161,14 @@ function displayLoginPage(){
     const usernameLogin = document.querySelector('#usernameLogin')
     const passwordLogin = document.querySelector('#passwordLogin')
     const loginButton = document.querySelector('#loginButton')
-    loginButton.addEventListener("click", login)
+    loginButton.addEventListener("click", ()=>{
+        if (usernameLogin.value == null || usernameLogin.value == "", passwordLogin.value == null || passwordLogin.value == ""){
+            modalEmptyUsernameOrPassword()
+            displayLoginPage()
+        }else{
+            login()
+        }
+    })
 }
 
 function displayRegisterPage(){
@@ -163,7 +179,12 @@ function displayRegisterPage(){
     const regPassword = document.querySelector("#regPassword")
     const regButton = document.querySelector("#register")
     regButton.addEventListener("click", ()=>{
-        register(regUsername.value, regPassword.value)
+        if (regUsername.value == null || regUsername.value =="", regPassword.value == null || regPassword.value =="" ){
+            modalEmptyUsernameOrPassword()
+            displayRegisterPage()
+        }else{
+            register(regUsername.value, regPassword.value)
+        }
     })
 
 }
@@ -199,23 +220,25 @@ function register(){
         password : regPassword.value
     }
 
-
     let bodySerialise = JSON.stringify(body)
 
     let fetchParams = {
         method : "POST",
         body: bodySerialise
-
     }
-
-
         fetch(url, fetchParams)
             .then(response=>response.json())
-            .then(data=>console.log(data))
-
-
-
-
+            .then(data=>{
+                if (data =="username already taken"){
+                    const modalUsernameTaken = document.querySelector("#modalUsernameTaken")
+                    const modal = new bootstrap.Modal(modalUsernameTaken)
+                    modal.show()
+                    displayRegisterPage()
+                }else{
+                    console.log(data)
+                    displayLoginPage()
+                }
+            })
 }
 
 function login(){
@@ -225,32 +248,25 @@ function login(){
         password : passwordLogin.value
     }
 
-
     let bodySerialise = JSON.stringify(body)
-
     let fetchParams = {
         headers:{"Content-Type":"application/json"},
         method : "POST",
         body: bodySerialise
-
     }
-
-
     fetch(url, fetchParams)
         .then(response=>response.json())
         .then(data=> {
 
             if(data.token){
+                console.log(data)
                 token = data.token
                 displayMessagesPage()
             }else{
+                const modalWrongUsernameOrPassword = document.querySelector("#modalWrongUsernameOrPassword")
+                const modal = new bootstrap.Modal(modalWrongUsernameOrPassword)
+                modal.show()
                 displayLoginPage()
-                alert("log in first to see messages")
             }
-
         })
-
-
-
 }
-
